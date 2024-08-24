1b33b506f2fd598cb36117d55b935503dd6a2513
Santierazo <santiagoerazoaragon@gmail.com>
2024-08-24 13:00:38 -0500

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -44,7 +44,7 @@ const userSchema = mongoose.Schema({
     },
     actividades: {
         type: [String],
-        enum: ['Meditación', 'Sonidos Relajantes', 'Ejercicios para dormir', 'Prácticas para Dormir'], 
+        enum: ['Meditación', 'Sonidos Relajantes', 'Ejercicios de respiración', 'Prácticas para Dormir'], 
     }
 });
 
