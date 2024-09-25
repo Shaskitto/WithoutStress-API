7bc06eeb6d58230e3d30ae973bb83e9646ed0978
Shaskitto <santiagoerazoaragon@gmail.com>
2024-09-25 17:05:01 -0500

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -12,9 +12,9 @@ router.get('/user/:id/profile-image', userController.getProfileImage);
 router.post('/upload', upload.single('profileImage'), userController.uploadImage);
 //router.get('/user/search-friend/:username', verifyToken, userController.searchFriend);
 router.post('/user/friends/request/:id', verifyToken, userController.sendRequest);
-router.get('/user/friends/requests/pending/:id', verifyToken, userController.getPendingRequests);
-router.post('/user/friends/requests/:friendId/accept', verifyToken, userController.acceptRequest);
-router.delete('/user/friends/requests/:friendId', verifyToken, userController.deleteRequest);
+router.get('/user/friends/request/pending/:id', verifyToken, userController.getPendingRequests);
+router.post('/user/friends/request/accept/:id', verifyToken, userController.acceptRequest);
+router.delete('/user/friends/request/delete/:id', verifyToken, userController.deleteRequest);
 //router.get('/user/friends/:friendId', verifyToken, userController.getFriendById);
 //router.delete('/user/friends/:friendId', verifyToken, userController.deleteFriend);
 
