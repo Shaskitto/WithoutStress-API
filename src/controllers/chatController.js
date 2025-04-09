3c665cd17c3a425b44baaeff1e283d8fd07ce32c
Santiago Erazo Aragón <103299493+Shaskitto@users.noreply.github.com>
2025-04-08 20:32:54 -0500

    Quitando validacion de accepted

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -1,6 +1,6 @@
-const chatSchema = require('../models/chatModel');
-const userSchema = require('../models/userModel');
-const { generateRoomId } = require('../middlewares/chatUtils');
+const chatSchema = require("../models/chatModel");
+const userSchema = require("../models/userModel");
+const { generateRoomId } = require("../middlewares/chatUtils");
 
 exports.getChat = async (req, res) => {
   const { user1, user2 } = req.params;
@@ -11,37 +11,30 @@ exports.getChat = async (req, res) => {
 };
 
 exports.sendMessage = async (req, res) => {
-    const { user1: sender, user2 } = req.params;
-    const { content } = req.body;
-  
-    try {
-      const senderUser = await userSchema.findById(sender);
-      if (!senderUser) {
-        return res.status(404).json({ error: 'Usuario remitente no encontrado.' });
-      }
-  
-      const isFriend = senderUser.friends.some(
-        (f) => f.friendId.toString() === user2 && f.status === 'accepted'
-      );
-  
-      if (!isFriend) {
-        return res.status(403).json({ error: 'No puedes enviar mensajes a este usuario.' });
-      }
-  
-      const roomId = generateRoomId(sender, user2);
-  
-      const message = new chatSchema({
-        roomId,
-        sender,
-        content,
-        timestamp: new Date()
-      });
-  
-      await message.save();
-  
-      res.status(201).json(message);
-    } catch (error) {
-      res.status(500).json({ error: 'Error al enviar el mensaje' });
+  const { user1: sender, user2 } = req.params;
+  const { content } = req.body;
+
+  try {
+    const senderUser = await userSchema.findById(sender);
+    if (!senderUser) {
+      return res
+        .status(404)
+        .json({ error: "Usuario remitente no encontrado." });
     }
-  };
-  
+
+    const roomId = generateRoomId(sender, user2);
+
+    const message = new chatSchema({
+      roomId,
+      sender,
+      content,
+      timestamp: new Date(),
+    });
+
+    await message.save();
+
+    res.status(201).json(message);
+  } catch (error) {
+    res.status(500).json({ error: "Error al enviar el mensaje" });
+  }
+};
